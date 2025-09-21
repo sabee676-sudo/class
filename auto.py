0a86c840d5b61540db5333d59b20539cfca6b704
sabee676-sudo <sabee676@gmail.com>
2025-09-21 01:40:27 -0600

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,19 @@
+class coche: 
+    def __init__(self,color,marca,modelo,tipo_motor,tipo_tras,):
+        self.color=color
+        self.marca=modelo
+        self.marca=marca
+        self.modelo=modelo
+        self.tipo_motor=tipo_motor
+        self.tipo_trans=tipo_tras
+
+    def avanzar (self):
+        print("el coche avanza")
+
+    def frena(self):
+        print("el coche si frena")
+
+    print ("te extraño")
+
+coche1 =coche()
+coche1.frena   
